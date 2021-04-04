8ca7c78c1a7c9de2eb9e53b7c3a1506faf4c27aa
Cyber-Jb <81779689+Cyber-Jb@users.noreply.github.com>
2021-04-04 15:38:36 +0100

    Add files via upload

---
+++ Csc104assign1.py
@@ -0,0 +1,36 @@
+#1. Code to display Name, and Department.
+Name="Yakubu John Bitrus"
+Dept="Computer Science"
+print("Name:-",Name,"\n","Department:-",Dept)
+
+#2. Assign a string 'Boo!' and variable 'Scare' then print the variable.
+Scare="Boo!"
+print(Scare)
+
+#3.Add two variables and assign the result to the third variable, print all the variable names.
+Var1=15
+Var2=3
+Var3=(Var1+Var2)
+print("Var1:",Var1,"\n","Var2:",Var2,"\n""Var3:",Var3)
+
+#4.Divide 12 by the decimal .5 and assign the result to the variable outcome and print the result.
+A=12
+B=0.5
+print("Result:",A/B)
+
+#5.Assign the sum of two numbers to a variable and print the variable.
+Sum=(Var1+Var2)
+print("Result:",Sum)
+
+#6.Write the code that finds the remainder when 8 is divided by 3.
+ValueA=8
+ValueB=3
+print("Remainder:",ValueA%ValueB)
+
+#7.First multiply 2 by 4 then add 4 and 2. then multiply the first result by the second.
+NumberA=4
+NumberB=2
+First_total=(NumberA*NumberB)
+Second_total=(NumberA+NumberB)
+Final_answer=(First_total*Second_total)
+print("Multiplied:",First_total,"\n","Sum:",Second_total,"\n","Final Answer:",Final_answer)
